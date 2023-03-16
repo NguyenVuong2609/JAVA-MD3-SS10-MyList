119a809b1760f98cb31f5f8f6d7f6807e13c2502
Nguyen Vuong 2609 <ducvuong.269@gmail.com>
2023-03-16 13:38:24 +0700

    1603

---
+++ src/Rikkei/academy/BT2_LinkedList/LinkedList.java
@@ -0,0 +1,33 @@
+package Rikkei.academy.BT2_LinkedList;
+
+import Rikkei.academy.TH2_LinkedList.MyLinkedList;
+
+public class LinkedList {
+    private Node head;
+    private int numNodes;
+    private class Node {
+        private Node next;
+        private Object data;
+
+        public Node(Object data) {
+            this.data = data;
+        }
+
+        public Object getData() {
+            return this.data;
+        }
+    }
+    public void add(int index, Object data) {
+        Node temp = head;
+        Node holder;
+
+        for (int i = 0; i < index - 1 && temp.next != null; i++) {
+            temp = temp.next;
+        }
+        holder = temp.next;
+        temp.next = new Node(data);
+        temp.next.next = holder;
+        numNodes++;
+    }
+
+}
